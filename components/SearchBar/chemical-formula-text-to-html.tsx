dff170c88ff3f6f2e55a7a174e65c8e8e5ddf69f
AlperBayraktar <alperbayraktar12@gmail.com>
2023-12-07 18:40:15 +0300

    removed codes

--- components/SearchBar/chemical-formula-text-to-html.tsx
+++
@@ -1,8 +0,0 @@
-const ChemicalFormulaTextToHTML: any = (formula: any) =>
-    formula
-        .split(/(\d+)/)
-        .map((token: any, idx: any) =>
-            idx % 2 ? <sub key={idx}>{token}</sub> : token
-        );
-
-export default ChemicalFormulaTextToHTML;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,4 @@
-import Head from "next/head";
-import { useState } from "react";
-import Script from "next/script";
-// import MainPage from "@/components/dfhdjhfj";
-
 const Home = () => {
-    const [ChemDoodle_, setChemDoodle_] = useState(undefined);
-
-    return (
-        <>
-            {/* <Head>
-                <title>3D Molekül Görüntüleyici</title>
-                <meta
-                    name="description"
-                    content="Eskişehir Fatih Fen Lisesi (EFFL) 10-D öğrencisi Alper Bayraktar
-                    tarafından geliştirilmiş 3D Molekül Görüntüleyici projesidir."
-                />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head> */}
-            {/* <Script
-                src="/chemdoodle/ChemDoodleWeb.js"
-                onReady={() => {
-                    setChemDoodle_(ChemDoodle);
-                }}
-            /> */}
-            {/* <MainPage ChemDoodle_={ChemDoodle_} /> */}
-            Hello world!
-        </>
-    );
+    return <div>Hello world!</div>;
 };
 export default Home;
