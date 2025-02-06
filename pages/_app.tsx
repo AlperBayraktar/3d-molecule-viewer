da0c761ad410fcc224fa180a7bde92d94ae74ae3
Alper Bayraktar <alperbayraktar12@gmail.com>
2025-02-06 21:03:10 +0300

    Enabled Vercel Web Analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/chakra-theme";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
